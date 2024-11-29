7dd0311ceba64e795e39ef6e6f921008d57b56f0
Blxee <aminedev57@gmail.com>
2024-11-29 13:20:03 +0100

    Added: frontend and backend wrapper for the xo engine

--- server.py
+++ server.py
@@ -1,10 +1,16 @@
-from flask import Flask
+from flask import Flask, jsonify, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def index():
+    return render_template("index.html", grid=[('X', 'O', ' ', 'X')[i % 4] for i in range(9)])
+
+@app.route("/api/mark", methods=['POST'])
+def mark():
+    print(f'marking {request.get_data(as_text=True)}')
+    return jsonify({'message': 'Ok'})
 
 if __name__ == "__main__":
+    app.debug = True
     app.run()
