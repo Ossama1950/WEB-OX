b03e42ee680041a66a2b28685fb45693bbf9c2ed
Ossama1950 <chucy0055@gmail.com>
2024-12-01 17:29:43 +0100

    more work on xo script

--- web_ox.py
+++ web_ox.py
@@ -10,13 +10,23 @@ def fil_spot(row,col):
     else:
         turn = "o"
     Grid[row][col] = turn
-    print(Grid)
+    for line in Grid:
+        print(line)
 
 
 def choose_spot():
     while True:
         row = int(input("choose a row:"))
         col = int(input("choose a col:"))
-        fil_spot(row,col)
-       
-choose_spot()
+        chosen = Grid[row][col]
+        if chosen != 0:
+            print("this spot is full" , chosen)
+            choose_spot()
+        else:
+            fil_spot(row,col)
+for element in Grid:
+    if 0 in element:
+        choose_spot()
+    else:
+       print("board is full")
+       exit()
